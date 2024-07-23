99c2f9b2d11f69c6e48bdab256ecb74c0920c868
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-23 20:32:39 +0300

    Update SumTable.js

--- SumTable.js
+++ SumTable.js
@@ -2,7 +2,7 @@ function sumTable() {
 let rows = document.querySelectorAll(`table tr`);
 let sum = 0;
 for (let i=1; i<rows.length-1; i++){
-let cost = rows[i].lastChild
+let cost = rows[i].lastChild;
 sum +=Number(cost.textContent)
 }
 document.getElementById(`sum`).textContent=sum;
