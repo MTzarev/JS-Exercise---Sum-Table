991ba995d563ee2fd68e8d3d0a23515570d72551
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-23 20:31:38 +0300

    Add files via upload

---
+++ SumTable.js
@@ -0,0 +1,9 @@
+function sumTable() {
+let rows = document.querySelectorAll(`table tr`);
+let sum = 0
+for (let i=1; i<rows.length-1; i++){
+let cost = rows[i].lastChild
+sum +=Number(cost.textContent)
+}
+document.getElementById(`sum`).textContent=sum;
+}
